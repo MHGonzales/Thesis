6887a971e07fc06414c1e7ae3adb14e5f7b80523
MHGonzales <matt_gonzales@dlsu.edu.ph>
2021-10-27 15:25:43 +0800

    Dobot
    Change  test.py to dobot intialization

--- Python/test.py
+++ Python/test.py
@@ -1,8 +1,11 @@
-print("Hello World")
-print("heloooo")
-print("banana")
-print("workworkwokr")
-print("aaaaaaa")
-print("Helpppp")
-x = 5
-print(x)
+import threading
+import DobotDllType as dType
+
+CON_STR = {
+    dType.DobotConnect.DobotConnect_NoError:  "DobotConnect_NoError",
+    dType.DobotConnect.DobotConnect_NotFound: "DobotConnect_NotFound",
+    dType.DobotConnect.DobotConnect_Occupied: "DobotConnect_Occupied"}
+    
+api = dType.load()
+status = dType.ConnectDobot(api,"",115200)[0]
+print("Connect Status:", CON_STR[status])
