5c376208ebc387badf149db6ea0a812ee7c229c3
MHGonzales <matt_gonzales@dlsu.edu.ph>
2021-12-06 23:21:32 +0800

    Update client.py

--- TCP Communication/client.py
+++ TCP Communication/client.py
@@ -2,7 +2,7 @@ import socket, threading
 
 def send():
     while True:
-        msg = input('nMe > ').encode('utf-8')
+        msg = input('\nMe > ').encode('utf-8')
         cli_sock.send(msg)
 
 def receive():
@@ -10,15 +10,15 @@ def receive():
         sen_name = cli_sock.recv(1024).decode('utf-8')
         data = cli_sock.recv(1024).decode('utf-8')
 
-        print('n' + str(sen_name) + ' > ' + str(data))
+        print('\n' + str(sen_name) + ' > ' + str(data))
 
 if __name__ == "__main__":   
     # socket
     cli_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # connect
-    HOST = '52.220.121.212' #change when starting new ngrok tunnel
-    PORT = 18465    #change when starting new ngrok tunnel
+    HOST = '52.220.121.212' #change when starting new tcp tunnel
+    PORT = 12448    #change when starting new tcp tunnel
     cli_sock.connect((HOST, PORT))     
     print('Connected to remote host...')
     uname = input('Enter your name to enter the chat > ').encode('utf-8')
