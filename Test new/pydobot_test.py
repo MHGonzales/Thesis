9a92ee71a1f1f840f3e201c1e135252fad032dd1
MHGonzales <matt_gonzales@dlsu.edu.ph>
2022-09-14 20:05:35 +0800

    commit

--- Test new/pydobot_test.py
+++ Test new/pydobot_test.py
@@ -9,12 +9,12 @@ port = available_ports[0].device
 device = pydobot.Dobot(port=port, verbose=True)
 
 (x, y, z, r, j1, j2, j3, j4) = device.pose()
-print(f'x:{x} y:{y} z:{z} j1:{j1} j2:{j2} j3:{j3} j4:{j4}')
+print('x:{x} y:{y} z:{z} j1:{j1} j2:{j2} j3:{j3} j4:{j4}')
 
-for i in range(2):
+for i in range(1):
     device.move_to(x + 20, y, z, r, wait=True)
     device.wait(1000)
     device.move_to(x, y, z, r, wait=True)
     device.wait(1000)
-
+    device.get_eio(6)
 device.close()
---
+++ Test new/servo_test.py
@@ -0,0 +1,25 @@
+import DobotDllType as dType
+
+CON_STR = {
+    dType.DobotConnect.DobotConnect_NoError:  "DobotConnect_NoError",
+    dType.DobotConnect.DobotConnect_NotFound: "DobotConnect_NotFound",
+    dType.DobotConnect.DobotConnect_Occupied: "DobotConnect_Occupied"}
+    
+api = dType.load()
+state = dType.ConnectDobot(api, "", 115200)[0]
+print("Connect status:",CON_STR[state])
+
+addr = 6
+frequency = 50
+
+dType.SetIOMultiplexing(api, addr, 2, 1)
+dType.dSleep(5000)
+
+for ii in range(3,13,1):
+    dutyCycle = float(ii)
+    print(dutyCycle)
+    dType.SetIOPWM(api, addr, frequency, dutyCycle,1)
+    dType.dSleep(5000)
+    print(dType.GetIOPWM(api, addr))
+    dType.dSleep(5000)
+dType.DisconnectDobot(api)
