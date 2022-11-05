d20a07a3263f21d699c07437924ab442dc5aabde
MHGonzales <matt_gonzales@dlsu.edu.ph>
2022-11-05 08:48:14 +0800

    commit

--- IK develop/fk3dof.py
+++ IK develop/fk3dof.py
@@ -1,37 +1,49 @@
+#from header import *
 import numpy as np
 import roboticstoolbox as rtb
-import spatialmath as sm
+from spatialmath import SE3
+from ikdof import *
 
-#change value to actual dobot link lengths
-l0 = 1
-l1 = 1
-l2 = 1
+def robot():
+    #reference frames translation and rotation
+    E1 = rtb.ET.tz(0.333)
+    E2 = rtb.ET.Rz()
+    E3 = rtb.ET.Ry()
+    E4 = rtb.ET.tz(0.316)
+    E5 = rtb.ET.Rz()
+    E6 = rtb.ET.tx(0.0825)
+    E7 = rtb.ET.Ry(flip=True)
+    E8 = rtb.ET.tx(-0.0825)
+    E9 = rtb.ET.tz(0.384)
+    E10 = rtb.ET.Rz()
+    E11 = rtb.ET.Ry(flip=True)
+    E12 = rtb.ET.tx(0.088)
+    E13 = rtb.ET.Rx(np.pi)
+    E14 = rtb.ET.tz(0.107)
+    E15 = rtb.ET.Rz()
 
-#reference frames translation and rotation
-e1 = rtb.ET.tz(l0)
-e2 = rtb.ET.Rz()
-e3 = rtb.ET.Ry()
-e4 = rtb.ET.tz(l1)
-e5 = rtb.ET.Ry()
-e6 = rtb.ET.tz(l2)
-#forward kinematics
-ets = rtb.ETS([e1, e2, e3, e4, e5, e6])
-print(f"The dobot has {ets.n} joints")
-#sample initial joing angles
-q = np.array([45, 45, 45])
 
-# Allocate the resulting forward kinematics array
-fk = np.eye(4) 
+    panda = E1 * E2 * E3 * E4 * E5 * E6 * E7 * E8 * E9 * E10 * E11 * E12 * E13 * E14 * E15
 
-# Now we must loop over the ETs in the Panda
-for et in ets:
-    if et.isjoint:
-        # This ET is a variable joint
-        # Use the q array to specify the joint angle for the variable ET
-        fk = fk @ et.A(q[et.jindex])
-    else:
-        # This ET is static
-        fk = fk @ et.A()
+    
+    #return ets object
+    return panda
 
-# Pretty print our resulting forward kinematics using an SE3 object
-print(sm.SE3(fk))
+qf= np.array([0.79,1.57,0])
+q0 = np.array([0,0,0])
+ets = robot()
+#Final pose test
+Tf = SE3.Trans(0.7, 0.2, 0.1) *SE3.OA([0,  1, 0], [0, 0, 1])
+print(Tf)
+rb = rtb.ERobot(ets)
+#initial pose test
+T=rb.fkine(np.zeros(7))
+print(T)
+#inv kinematics
+sol = rb.ikine_LMS(Tf,ilimit = 3100)
+print(sol)
+
+print(rb.fkine(sol.q))
+
+
+
