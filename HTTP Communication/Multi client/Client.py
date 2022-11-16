6878b4e57189c1fab8b9348f7e472152bb740047
eriii1 <erika_kimberly_go@dlsu.edu.ph>
2022-11-16 17:18:30 +0800

    Update Client.py

--- HTTP Communication/Multi client/Client.py
+++ HTTP Communication/Multi client/Client.py
@@ -1,186 +1,57 @@
-#!/usr/bin/env python
-import sys
-import socket
-import time
-import threading
-from threading import Thread
-from SocketServer import ThreadingMixIn
+import socket, select, string, sys, math
 
+#Helper function (formatting)
+def display() :
+	you="\33[33m\33[1m"+" You: "+"\33[0m"
+	sys.stdout.write(you)
+	sys.stdout.flush()
 
+def main():
 
-
-class ServerThread(Thread):
- 
-    def __init__(self,socket):
-        Thread.__init__(self)
-        self.socket = socket
-        
-     #   print "New thread started for write"
-        
-        
- 
- 
-    def run(self):
-        print "send" 
-        
-        while True:
-            starttime = time.time()
-        
-            
-            command = raw_input(" Enter command: ")
-            
-            
-                
-            curtime = time.time()
-        
-            if  curtime - starttime > float(TIME_OUT):     #Client tiems itself out after TIME_OUT idle time
-                print " Your session has been timed out! Please log in again :(" 
-                self.socket.close()
-                sys.exit()
-            else: 
-                
-                self.socket.send(command)
-           
-                ack = self.socket.recv(BUFFER_SIZE)
-                print ack
-                if ack == "logged out":
-                    log = 1
-                    self.socket.close()
-                    sys.exit()
-                    
-                elif ack == "user already exists":
-                    print "user alread exists -.-"  
-                    self.socket.close()
-                    sys.exit()
-          
-             
-               
-                
-                
-                            
-      
-        
-        
-class ServerThreadread(Thread):
- 
-    def __init__(self,socket):
-        Thread.__init__(self)
-        self.socket = socket
-        
-      #  print "New thread started for chat display"
-      
-       
- 
- 
-    def run(self):
-        
-        s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s2.connect((TCP_IP, TCP_PORT2))
-        welcomemsg = s2.recv(BUFFER_SIZE)
-        chat = "initial"
-        print welcomemsg
-        
-        while True:
-            if log == 0:
-              #  print "inside loop"
-                chat=s2.recv(BUFFER_SIZE)
-                print chat
-                time.sleep(5)
-                
-            if log == 1:
-              #  print "going to exit"
-                s2.close()
-                sys.exit() 
-       
-           
-                              
-        
-
-
- 
- 
-TCP_IP = sys.argv[1]
-TCP_PORT = int(sys.argv[2])
-TCP_PORT2 = 125
-BUFFER_SIZE = 1024
-threads = []
-global log
-log = 0
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-TIME_OUT = s.recv(BUFFER_SIZE)   #Server exchanges tmeout details with client at the start of every socket
-count = [1, 2, 3]
-status = 0
-while status == 0:
-    number = 0
-    username = raw_input("Enter username: ")
-    s.send(username)
-    usernamecheck = s.recv(BUFFER_SIZE)
-    if ( usernamecheck == "invalid login" ):
-        print "Invalid username , enter details again "
-        status =0
-        
-        continue
+    if len(sys.argv)<2:
+        host = input("Enter host ip address: ")
     else:
-        if usernamecheck == " blocked ":
-            status = 2
-            print "I said you have been blocked for 60 seconds. Be patient -.-"
-            sys.exit()
-        elif usernamecheck == "same user":
-            status = 1
-            print "User is already online. Who are you ?"
-            sys.exit()
-        else:    
-            while status == 0:
-                password = raw_input("Enter password: ") 
-                s.send(password)
-                passwordcheck = s.recv(BUFFER_SIZE)
-                if ( passwordcheck == "invalid password" ):
-                    
-                    status = 0
-                    number = number + 1
-                    if number == 3:
-                        status = 2    
-                        break
-                
-                    else:
-                        print " Invalid password , enter details again "
-                        continue
-                else:
-                    status = 1        
-                        
-                           
-if number == 3 and status == 2:
-    print "I don't know. My instructor has asked me to block you for 60s." 
-    sys.exit()
-            
-         
+        host = sys.argv[1]
 
-        
-if ( status == 1 ):
-    print "logged in"
-    try:
+    port = 5001
     
-        newthread = ServerThread(s)
-        newthread.daemon = True
-        newthread2 = ServerThreadread(s)
-        newthread2.daemon = True
-        newthread.start()
-        newthread2.start()
-        threads.append(newthread)
-        threads.append(newthread2)
-        while True:
-            for t in threads:
-                t.join(600)
-                if not t.isAlive():
-                    break
-            break        
-            
-            
-    except KeyboardInterrupt:
-        command = "logout"
-        s.send(command)
+    #asks for user name
+    name=input("\33[34m\33[1m CREATING NEW ID:\n Enter username: \33[0m")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+    
+    # connecting host
+    try :
+        s.connect((host, port))
+    except :
+        print ("\33[31m\33[1m Can't connect to the server \33[0m")
         sys.exit()
-    
-   
-            
+
+    #if connected
+    s.send(name)
+    display()
+    while 1:
+        socket_list = [sys.stdin, s]
+        
+        # Get the list of sockets which are readable
+        rList, wList, error_list = select.select(socket_list , [], [])
+        
+        for sock in rList:
+            #incoming message from server
+            if sock == s:
+                data = sock.recv(4096)
+                if not data :
+                    print ('\33[31m\33[1m \rDISCONNECTED!!\n \33[0m')
+                    sys.exit()
+                else :
+                    sys.stdout.write(data)
+                    display()
+        
+            #user entered a message
+            else :
+                msg=sys.stdin.readline()
+                s.send(msg)
+                display()
+
+if __name__ == "__main__":
+    main()
