4695fb48f67027a7ea03caa331314bb9edcaa47f
MHGonzales <matt_gonzales@dlsu.edu.ph>
2022-11-28 17:54:01 +0800

    import

--- Development/control.py
+++ Development/control.py
@@ -3,3 +3,15 @@ from numpy import pi
 from spatialmath import SE3
 import roboticstoolbox as rtb
 from dobject import Dobot
+
+
+rb = Dobot()
+q0 = rb.qr
+
+def IKine(q0,x:float, y:float ,z:float ):
+    Tf = SE3.Trans(x ,y ,z) *SE3.OA([0,  0, 1], [1, 0, 0])
+    sol = rb.ikine_LMS(Tf,q0,)
+
+if __name__ == "__main__":
+    while True:
+        x =1
