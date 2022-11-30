d8a9a717a5f83966a6747933220866a5cdddd6b5
MHGonzales <matt_gonzales@dlsu.edu.ph>
2022-11-30 20:19:34 +0800

    to do tommorow

--- Development/control.py
+++ Development/control.py
@@ -19,33 +19,66 @@ def jpos(q0:np.ndarray,x:float,y:float,z:float):
 
 #determine joint trajectory
 def qpath(qn,q0):
-    qtraj = rb.jtraj(qn,q0,100)
+    qtraj = rb.jtraj(qn,q0,75)
     return qtraj.q
 
 #convert rad to deg
-def r2g(rad:float):
+def r2d(rad:float):
     deg:float
-
+    #erika
     return deg
 
-#convert rad to pwm
+#convert deg to pwm
 def r2p(rad:float):
     pwm:float
-
+    #erika
     return pwm
 
 #update joint angles
-def updateje():
+#input sol.q
+def setje():
+    #loop to move through qtraj
+    #dtype.SetPTPCmd(api,4,j1,j2,j3) 
     exit
 
 #update wrist position
-def updatejw():
+def setjw(pwm:float):
+    
+    #dType.SetIOMultiplexing(api, 4, 2, 1)
+    #dType.SetIOMultiplexing(api, 6, 2, 1)
+    #dType.SetIOMultiplexing(api, 8, 2, 1)
+
+    #loop to move through qtraj
+    #dType.SetIOPWM(api, 4, 50, pwm,1)
+    #dType.SetIOPWM(api, 6, 50, pwm,1)
+    #dType.SetIOPWM(api, 8, 50, pwm,1)
+
     exit
 
 def main():
+    #branch based on button pressed
+    #obtain new coordinates
+        #get x,y,z
+    #obtain joint angles
+        #qn = jpos(q0,x,y,z)
+        #qtraj = qpath(qn,q0)
+    #convert rad to degrees
+        #qn = r2d(qtraj)
+    #convert degrees to pwm
+        #qne = r2(qtraj)
+    #set joint angles in robot using DOBOT API
+        #setje(qtraj) and setjw(qtraj)
+    #update new angles to be current angles
+        #q0 = qn
+    #repeat
     exit
 
 if __name__ == "__main__":
     api = dType.load()
+    #initialize starting joint angles
+        #q0 = rb.qz
+        #dtype.SetJointAngle(api,4,q0[0],q0[1],q0[2],current_pose[7],1)
+    #while True:
+        #keyboard event go to main
     main()
     
