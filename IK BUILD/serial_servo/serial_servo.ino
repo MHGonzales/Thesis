e0931ef99176ed130de417191a87cb3d62c08a58
MHGonzales <matt_gonzales@dlsu.edu.ph>
2023-02-02 15:09:23 +0800

    edit

--- IK BUILD/serial_servo/serial_servo.ino
+++ IK BUILD/serial_servo/serial_servo.ino
@@ -1,23 +1,33 @@
 #include <Servo.h>
-#include <MPU6050_light.h>
+
                                         
-Servo servo_1,servo_2,servo_3; // servo controller (multiple can exist)
+Servo servo_1,servo_2,servo_3;
+
+ // servo controller (multiple can exist)
 int pos_1,pos_2,pos_3 = 0;    // servo starting position
 int j1,j2,j3 = 0;
+
 
 void setup() {
   servo_1.attach(9,520,2465);
   servo_2.attach(10,625,2540);
   servo_3.attach(6,520,2460); // start servo control
   Serial.begin(9600); // start serial monitor
+
+
   servo_1.write(pos_1);
   servo_2.write(pos_2);
-  servo_3.write(pos_3); // move servo to 0 degrees
+  servo_3.write(pos_3); 
+  
+  // move servo to 0 degrees
   Serial.println("Positioned at 0 Degrees");
   Serial.println("Input Desired Angle and Press Enter");
+
+ 
 }
 
 void loop() {
+  mpu.update();
   while (Serial.available()){
     String rxString = "";
     String strArr[3];
@@ -53,39 +63,47 @@ void loop() {
         {
           pos_1++;
           servo_1.write(pos_1);
-          delay(15);   
+          delay(10);   
         }
         if (pos_2 < j2)
         {
           pos_2++;
           servo_2.write(pos_2);
-          delay(15);   
+          delay(10);   
         }
         if (pos_3 < j3)
         {
           pos_3++;
           servo_3.write(pos_3);
-          delay(15);   
+          delay(10);   
         }  
         if (pos_1 > j1)
         {
           pos_1--;
           servo_1.write(pos_1);
-          delay(15);   
+          delay(10);   
         }
         if (pos_2 > j2)
         {
           pos_2--;
           servo_2.write(pos_2);
-          delay(15);
+          delay(10);
         }
         if (pos_3 > j3)
         {
           pos_3--;
           servo_3.write(pos_3);
-          delay(15);
-        }        
+          delay(10);
+        } 
+             
     } // convert angle and write servo // delay for maximum speed
+    
+    Serial.print("Pitch : ");
+    Serial.print(mpu.getAngleX());
+    Serial.print("\tYaw : ");
+    Serial.print(mpu.getAngleY());
+    Serial.print("\tRoll : ");
+    Serial.println(mpu.getAngleZ()); 
     pos_1 = j1;
     pos_2 = j2;
     pos_3 = j3;
