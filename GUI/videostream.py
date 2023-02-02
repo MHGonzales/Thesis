d1f07877d2cb5a5f757ce5628f8299a33a4486d8
eriii1 <erika_kimberly_go@dlsu.edu.ph>
2023-02-02 17:42:21 +0800

    input js on console

--- GUI/videostream.py
+++ GUI/videostream.py
@@ -7,6 +7,7 @@ os.system("start \"\" http://1.tcp.ap.ngrok.io:21694")
 app = Flask(__name__)
 video = cv2.VideoCapture(3)
 video2 = cv2.VideoCapture(0)
+bg = cv2.imread('WEB.png', cv2.IMREAD_COLOR) 
 
 @app.route("/")
 def index():
@@ -31,11 +32,11 @@ def gen(video2):
         
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-        
 
 @app.route('/ARM VIEW')
 def video_feed():
     global video
+    
     return Response(gen(video),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 @app.route('/LAB VIEW')
@@ -43,5 +44,10 @@ def video_feed2():
     global video2
     return Response(gen(video2),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/CONSOLE')
+def control():
+    return render_template('console.html')
+
 if __name__ == '__main__':
     app.run(host='localhost', port=2037, threaded=True)
