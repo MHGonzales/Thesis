060630b371c8baf622c267183781faa5557ec28f
MHGonzales <matt_gonzales@dlsu.edu.ph>
2023-02-20 18:01:32 +0800

    Update serial_servo.ino
    integrated mpu 6050 read with servo movement

--- IK BUILD/serial_servo/serial_servo.ino
+++ IK BUILD/serial_servo/serial_servo.ino
@@ -1,5 +1,8 @@
 #include <VarSpeedServo.h>
+#include "Wire.h"
+#include <MPU6050_light.h>
 
+MPU6050 mpu(Wire);
                                         
 VarSpeedServo servo_1,servo_2,servo_3,grip;
 
@@ -9,7 +12,7 @@ int j1,j2,j3 = 0;
 int pos_1 = 90;
 int pos_2 = 90;
 int pos_3 = 85;
-
+unsigned long timer = 0;
 
 void setup() {
 
@@ -19,15 +22,26 @@ void setup() {
   servo_2.attach(10,544,2400);
   servo_3.attach(6,500,2400); // start servo control
   Serial.begin(9600); // start serial monitor
-
+  Wire.begin();
 
   servo_1.write(pos_1);
   servo_2.write(pos_2);
   servo_3.write(pos_3); 
   
   // move servo to 0 degrees
-  Serial.println("Positioned at 0 Degrees");
-  Serial.println("Input Desired Angle and Press Enter");
+  delay(5000);
+  Serial.println("Positioned at Home Position");
+
+  byte status = mpu.begin();
+  Serial.print(F("MPU6050 status: "));
+  Serial.println(status);
+  while(status!=0){ }
+  
+  Serial.println(F("Calculating offsets, do not move MPU6050"));
+  delay(1000);
+  // mpu.upsideDownMounting = true; // uncomment this line if the MPU6050 is mounted upside-down
+  mpu.calcOffsets(); // gyro and accelero
+  Serial.println("Done!\n");
 
  
 }
@@ -73,14 +87,25 @@ void loop() {
     j3 = strArr[2].toInt();
     
     
-    servo_1.slowmove(j1,12); 
+
+    servo_1.slowmove(j1+90,12); 
       //delay(15);
-    servo_2.slowmove(j2,12);
+    servo_2.slowmove(j2+90,12);
       //delay(15);
-    servo_3.slowmove(j3,12);             // tell servo to go to position in variable 'j1'
+    servo_3.slowmove(j3+90,12);             // tell servo to go to position in variable 'j1'
     //delay(15);
     
     
+  }
+  mpu.update();
+  if((millis()-timer)>10){ // print data every 10ms
+    Serial.print("X : ");
+    Serial.print(mpu.getAngleX());
+    Serial.print("\tY : ");
+    Serial.print(mpu.getAngleY());
+    Serial.print("\tZ : ");
+    Serial.println(mpu.getAngleZ());
+    timer = millis();
   }
   
 }
