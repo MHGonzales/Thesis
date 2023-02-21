18774db4f03ec878ba41248f9d0aee9af1dee83e
MHGonzales <matt_gonzales@dlsu.edu.ph>
2023-02-21 15:26:38 +0800

    Update serial_servo.ino
    added pid

--- IK BUILD/serial_servo/serial_servo.ino
+++ IK BUILD/serial_servo/serial_servo.ino
@@ -8,11 +8,20 @@ VarSpeedServo servo_1,servo_2,servo_3,grip;
 
  // servo controller (multiple can exist)
   // servo starting position
-int j1,j2,j3 = 0;
-int pos_1 = 90;
-int pos_2 = 90;
-int pos_3 = 85;
-unsigned long timer = 0;
+double set_j1,set_j2,set_j3 = 90;
+double i_pitch,i_roll,i_yaw;
+double e_1,e_2,e_3;
+unsigned long timer,currentTime,previousTime;
+
+
+double elapsedTime;
+double lastError1,lastError2,lastError3;
+double cumError1,cumError2,cumError3, rateError1,rateError2,rateError3;
+double out1,out2,out3;
+
+double kp = 2;
+double ki = 5;
+double kd = 1;
 
 void setup() {
 
@@ -24,9 +33,9 @@ void setup() {
   Serial.begin(9600); // start serial monitor
   Wire.begin();
 
-  servo_1.write(pos_1);
-  servo_2.write(pos_2);
-  servo_3.write(pos_3); 
+  servo_1.write(set_j1);
+  servo_2.write(set_j2);
+  servo_3.write(set_j3); 
   
   // move servo to 0 degrees
   delay(5000);
@@ -82,30 +91,72 @@ void loop() {
 
 
    
-    j1 = strArr[0].toInt();
-    j2 = strArr[1].toInt();
-    j3 = strArr[2].toInt();
+    set_j1 = strArr[0].toFloat()+90;
+    set_j2 = strArr[1].toFloat()+90;
+    set_j3 = strArr[2].toFloat()+90;
     
     
 
-    servo_1.slowmove(j1+90,12); 
+    servo_1.slowmove(set_j1,12); 
       //delay(15);
-    servo_2.slowmove(j2+90,12);
+    servo_2.slowmove(set_j2,12);
       //delay(15);
-    servo_3.slowmove(j3+90,12);             // tell servo to go to position in variable 'j1'
+    servo_3.slowmove(set_j3,12);             // tell servo to go to position in variable 'j1'
     //delay(15);
     
     
   }
   mpu.update();
-  if((millis()-timer)>10){ // print data every 10ms
-    Serial.print("X : ");
-    Serial.print(mpu.getAngleX());
-    Serial.print("\tY : ");
-    Serial.print(mpu.getAngleY());
-    Serial.print("\tZ : ");
-    Serial.println(mpu.getAngleZ());
+  i_pitch = mpu.getAngleX();
+  i_roll = mpu.getAngleY();
+  i_yaw = mpu.getAngleZ();
+  if((millis()-timer)>100
+  ){ // print data every 10ms
+    Serial.print("pitch : ");
+    Serial.print(i_pitch);
+    Serial.print("\troll : ");
+    Serial.print(i_roll);
+    Serial.print("\tyaw : ");
+    Serial.println(i_yaw);
     timer = millis();
   }
   
+}
+double comp_pid(float i_pitch,float i_roll,float i_yaw,float set_j1, float set_j2,float set_j3){
+  currentTime = millis();
+
+  elapsedTime = (double)(currentTime - previousTime);
+  
+  //Errors
+
+  e_1 = set_j1 - i_yaw;
+  e_2 = set_j2 - i_roll;
+  e_3 = set_j3 - i_pitch;
+
+  //Integral Errors
+  cumError1 += e_1 *elapsedTime;
+  cumError2 += e_2 * elapsedTime;
+  cumError3 += e_3 * elapsedTime;
+
+
+  //Derivative Errors
+  rateError1 = (e_1-lastError1)/elapsedTime;
+  rateError2 = (e_2-lastError2)/elapsedTime;
+  rateError3 = (e_3-lastError3)/elapsedTime;
+
+  //New output
+  double out1 = kp*e_1 + ki*cumError1 + kd*rateError1;
+  double out2  = kp*e_2 + ki*cumError2 + kd*rateError2;
+  double out3  = kp*e_3 + ki*cumError3 + kd*rateError3;
+
+  lastError1 = e_1;
+  lastError2 = e_2;
+  lastError3 = e_3;
+
+  previousTime = currentTime;
+
+}
+
+double comp_newangle(){
+
 }
