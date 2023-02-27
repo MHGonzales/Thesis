3f7abba55eb86c868f34024bb606ab2a930123d0
MHGonzales <matt_gonzales@dlsu.edu.ph>
2023-02-27 10:23:43 +0800

    servo tune file, servo serial test file

---
+++ test/arduino_test/servo_tune/servo_tune.ino
@@ -0,0 +1,24 @@
+
+#include <Servo.h>
+
+Servo servo;
+int pos=180;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  servo.attach(9,544,2500);//default min:544 max: 2400 //yaw 544,2520 //pitch 544, 2500
+  servo.write(pos);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(Serial.available()){
+    pos = Serial.parseInt();
+    Serial.println(pos);
+    servo.write(pos);
+    break;
+  }
+  
+  
+}
