97d6efd666c9b4d1f2f8ed5ac6cf8af077b4fb1b
MHGonzales <matt_gonzales@dlsu.edu.ph>
2023-03-17 17:32:08 +0800

    Update videostream.py
    changed COM port

--- backend_web/videostream.py
+++ backend_web/videostream.py
@@ -18,7 +18,7 @@ from serial import Serial as sr
 
 print("Import Success !!")
 
-ad = sr('COM17',9600) #Nano com
+ad = sr('COM16',9600) #Nano com
 rb = Dobot()
 
 os.system("start \"\" http://1.tcp.ap.ngrok.io:21694")
@@ -680,7 +680,7 @@ if __name__ == '__main__':
     l = 0
     f = 0
     api = dType.load()
-    dType.ConnectDobot(api, "COM16", 115200) #Dobot COM
+    dType.ConnectDobot(api, "COM18", 115200) #Dobot COM
     #dType.SetIOMultiplexing(api, 4, 2, 1)
     #global current_pose
     current_pose=dType.GetPose(api)
